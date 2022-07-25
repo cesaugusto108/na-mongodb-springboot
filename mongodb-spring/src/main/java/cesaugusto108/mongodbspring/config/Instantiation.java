f38276c8cc3871c53cae0cc4823bd017319040da
Cesar Augusto <ces.augusto108@gmail.com>
2022-07-25 10:21:26 -0300

    Inserting objects into database

---
+++ mongodb-spring/src/main/java/cesaugusto108/mongodbspring/config/Instantiation.java
@@ -0,0 +1,24 @@
+package cesaugusto108.mongodbspring.config;
+
+import cesaugusto108.mongodbspring.domain.entities.User;
+import cesaugusto108.mongodbspring.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+    }
+}
